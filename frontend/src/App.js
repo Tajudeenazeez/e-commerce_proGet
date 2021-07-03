1116229cfdfef0426e1cd60b26d1a46554b6a368
tajudeen <tajudeenomotayo3@gmail.com>
2021-07-02 20:10:45 -0700

    screen feature added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {Container} from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screen/HomeScreen';
+
 
 function App() {
   return (
     <>
-      <h1>welcom</h1>
+      <Header/> 
+      <main className='py-3'>
+        <Container>
+           <HomeScreen/>
+            
+        </Container>
+      </main>
+     
+      <Footer/>
     </>
-   
 
   
 
